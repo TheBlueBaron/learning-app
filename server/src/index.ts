1048e928dd7f078d5ee056828ef295eabb08a903
TheBlueBaron <ronniehughjames@protonmail.com>
2025-01-27 19:46:34 +0000

    feat(ui, server): add user settings page

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,14 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
+import {
+  clerkMiddleware,
+  createClerkClient,
+  requireAuth,
+} from "@clerk/express";
 /* ROUTE IMPORTS */
 import courseRoutes from "./routes/courseRoutes";
+import userClerkRoutes from "./routes/userClerkRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -17,6 +23,10 @@ if (!isProduction) {
   dynamoose.aws.ddb.local();
 }
 
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
+
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -25,6 +35,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(clerkMiddleware());
 
 /* ROUTES */
 app.get("/", (req, res) => {
@@ -32,6 +43,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/courses", courseRoutes);
+app.use("/users/clerk", requireAuth(), userClerkRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3000;
