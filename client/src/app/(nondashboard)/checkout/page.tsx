2994c534d914903e3ef384d85ac754d7cb10e0e1
TheBlueBaron <ronniehughjames@protonmail.com>
2025-02-06 19:27:51 +0000

    feat(client, server): initial checkout page setup

---
+++ client/src/app/(nondashboard)/checkout/page.tsx
@@ -0,0 +1,32 @@
+import Loading from "@/components/Loading";
+import WizardStepper from "@/components/WizardStepper";
+import { useUser } from "@clerk/nextjs";
+import React from "react";
+
+const CheckoutWizard = () => {
+  const { isLoaded } = useUser();
+
+  if (!isLoaded) return <Loading />;
+
+  const renderStep = () => {
+    switch (checkoutStep) {
+      case 1:
+        return "checkout details page";
+      case 2:
+        return "payment page";
+      case 3:
+        return "completion page";
+      default:
+        return "checkout details page";
+    }
+  };
+
+  return (
+    <div className="checkout">
+      <WizardStepper currentStep={checkoutStep} />
+      <div className="checkout__content">{renderStep()}</div>
+    </div>
+  );
+};
+
+export default CheckoutWizard;
